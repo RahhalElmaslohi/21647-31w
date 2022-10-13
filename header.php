f49ada25a8be751ab4d16a84dd0328a4ebe18cf9
RahhalElmaslohi <rahhalelmaslohi@gmail.com>
2022-10-12 22:57:15 -0400

    ajout de nav menu au header.php

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
- * @package underscores
+ * @package underscore
  */
 
 ?>
@@ -23,20 +23,32 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
+	<header id="masthead" class="site__header">
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			
-			<h1 class="site-title">
+	<?php  /* Affichage du menu principal */
+			wp_nav_menu(array(
+			"menu" => "principal",
+			"container" => "nav",
+			"container_class" => "menu__principal")); ?>
+
+		<div class="site__branding">
+			<h1 class="site__title">
 				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
 			</h1>
-				
-			<?php
-			$underscores_description = get_bloginfo( 'description', 'display' );
-			if ( $underscores_description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $underscores_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
+		<?php
+			$underscore_description = get_bloginfo( 'description', 'display' );
+			if ( $underscore_description || is_customize_preview() ) : ?>
+			<p class="site__description"><?php echo $underscore_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+			<?php endif; ?>	
 		</div><!-- .site-branding -->
 
-		
 	</header><!-- #masthead -->
+	<aside class="widget__area">
+		<h2>Menu secondaire</h2>
+		<?php wp_nav_menu(array(
+					"menu" => "aside",
+					"container" => "nav",
+					"container_class" => "menu__aside"
+		));
+		?>
+	</aside>
