b3c0df7427d32bc60651aceff5e09734b8d40080
RahhalElmaslohi <rahhalelmaslohi@gmail.com>
2022-11-03 07:57:38 -0400

    ajout du fichier caterory-cours.php

--- category-cours.php
+++ category-cours.php
@@ -22,7 +22,7 @@
 				the_post(); ?>
                 <h1><a href="<?php the_permalink(); ?>">
                 <?php the_title(); ?></a></h1>
-                
+                <h2>Durée de cours<?php the_field('duree'); ?></h2>
                 <?php the_content(null, true); ?>
                 
             <?php endwhile; ?>
