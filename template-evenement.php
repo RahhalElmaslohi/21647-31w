d53a310c6ab545f4143c81e234e5fa405047a44f
RahhalElmaslohi <rahhalelmaslohi@gmail.com>
2022-11-23 22:50:07 -0500

    amelioration du style

--- template-evenement.php
+++ template-evenement.php
@@ -14,7 +14,8 @@
    <?php if (have_posts()): the_post(); ?>
         <?php the_title() ?>
         <?php the_content() ?>
-        <p>venez nous joindre au:<?php the_field('adresse'); ?></p>          
+        <p>venez nous joindre au:<?php the_field('adresse'); ?></p>
+        <p>Heure du présentation:<?php the_field('date-heure'); ?></p>           
    <?php endif ?>
 </main>
 <?php get_footer() ?>
